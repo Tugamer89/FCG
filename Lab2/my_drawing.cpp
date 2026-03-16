3e9b78158bcd69399f3430aab427cdda0cc1a33d
Tuga <61603718+Tugamer89@users.noreply.github.com>
2026-03-16 15:48:54 +0100

    Add initial implementation of drawing application with SFML
    - Created README.md with build instructions using CMake.
    - Implemented main drawing application in my_drawing.cpp.
    - Set up basic GUI state management and event handling.
    - Added placeholder functions for handling various events and rendering graphics.

---
+++ Lab2/my_drawing.cpp
@@ -0,0 +1,118 @@
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <math.h>
+
+////////////////////////////////////////////////////////////
+/// Utility
+
+template <typename T1, typename T2>
+double dist(sf::Vector2<T1> p1, sf::Vector2<T2> p2)
+{
+    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+}
+
+const sf::Color GRAY(128, 128, 128, 255);
+
+///
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+/// GUI state
+
+struct State
+{
+    sf::RenderWindow window;
+
+    State(unsigned w, unsigned h, const std::string &title)
+    {
+        window = sf::RenderWindow(sf::VideoMode({w, h}), title);
+    }
+};
+
+///
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+/// Callback functions
+void handle(const sf::Event::Closed &, State &gs)
+{
+    gs.window.close();
+}
+
+void handle(const sf::Event::TextEntered &textEnter, State &gs)
+{
+    // TODO
+}
+
+void handle(const sf::Event::KeyPressed &keyPressed, State &gs)
+{
+    // TODO
+}
+
+void handle(const sf::Event::MouseMoved &mouseMoved, State &gs)
+{
+    // TODO
+}
+
+void handle(const sf::Event::MouseButtonPressed &mouseBP, State &gs)
+{
+    // TODO
+}
+
+void handle(const sf::Event::MouseButtonReleased &, State &gs)
+{
+    // TODO
+}
+
+void handle(const sf::Event::Resized &resized, State &gs)
+{
+    sf::FloatRect visibleArea({0.f, 0.f}, sf::Vector2f(resized.size));
+    gs.window.setView(sf::View(visibleArea));
+}
+
+template <typename T>
+void handle(const T &, State &gs)
+{
+    // All unhandled events will end up here
+}
+///
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+/// Graphics
+void doGUI(State &gs)
+{
+    // TODO: here code to display the menus
+}
+
+void doGraphics(State &gs)
+{
+    gs.window.clear();
+    doGUI(gs);
+
+    // TODO: add here code to display shapes in your canvas
+
+    gs.window.display();
+}
+///
+////////////////////////////////////////////////////////////
+
+int main()
+{
+    State gs(800, 600, "Draw shapes");
+    gs.window.setFramerateLimit(50);
+
+    while (gs.window.isOpen()) // main loop
+    {
+        // event loop and handler through callbacks
+        gs.window.handleEvents([&gs](const auto &event) { handle(event, gs); });
+        // Show update
+        doGraphics(gs);
+    }
+}
